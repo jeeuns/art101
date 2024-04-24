dcf9fe8adbe0cb3ba2f15791781c65fda7ee0dbf
jeeun <85708606+jeeuns@users.noreply.github.com>
2024-04-23 18:07:28 -0700

    Lab5
    Edited CSS file
    And created JS file for Car Model/Make

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Regina Kim <rejikim@ucsc.edu>
+// Date: 4/23/2024
 
-// Constants
+// Variables
+make = "Toyota";
+model = "Prius";
+color = "White";
+year = 2012;
 
-// Functions
+//calculate
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+age = 2024 - year;
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+
