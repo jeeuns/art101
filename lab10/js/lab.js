f4fbd7da10a213d98cd260667848c4998c167491
jeeun <85708606+jeeuns@users.noreply.github.com>
2024-05-14 02:44:50 -0700

    Lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,21 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+   lab.js - This simple JavaScript/jQuery script appends new elements to an output div
 
-// Constants
+   Requirements: jQuery must be loaded for this script to work.
 
-// Functions
+   Author: Regina Kim
+   Date: 2024
+*/
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function generateRandomText() {
+  const text = "The stars whispered secrets of the universe, casting a celestial glow on the midnight horizon. As the night deepened, the gentle hum of the cosmos could be felt in the cool breeze, while the moonlight danced gracefully on the surface of the tranquil lake. Beneath the vast expanse of the sky, the world seemed to pause, immersed in the serene beauty of the nocturnal symphony.";
+  const min = 5;
+  const max = 50;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// click listener for button
+$("#make-convo").click(function(){
+  // get new fake dialogue
+  const newText = generateRandomText();
+  // append a new div to our output div
+  $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
