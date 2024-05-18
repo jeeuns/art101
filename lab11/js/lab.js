f5937c1f1a22697c7cc6d2cae822871965d1a2bc
jeeun <85708606+jeeuns@users.noreply.github.com>
2024-05-17 21:20:14 -0700

    Lab 11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js
+// Author: Regina Kim
+// Date: 5.17.24
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// click listener for button
+$("#submit").click(function(){
+  // get value of input field
+  const userName = $("#user-name").val();
 
-// let's get this party started
-main();
+  // now let's sort it
+  userNameSorted = sortString(userName);
+
+  // append a new div to our output div
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+
+});
