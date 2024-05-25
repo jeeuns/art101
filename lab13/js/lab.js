f99ca207ada4a6e33212d7d134698b7361dd8f7a
jeeun <85708606+jeeuns@users.noreply.github.com>
2024-05-24 23:10:44 -0700

    Lab13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,21 +1,62 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Regina Kim
+// Date: 5/24/24
 
-// Constants
+maxFactors = 4;
 
-// Functions
+outputEl = document.getElementById("output");
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function getFactorObj() {
+  var factorObj = {};
+  for (var factor=0; factor < maxFactors; factor++) {
+    numId = 'num' + factor;
+    textId = 'text' + factor;
+    numValue = document.getElementById(numId).value;
+    textValue = document.getElementById(textId).value;
+    console.log(factor + ") num:", numValue, "text:", textValue)
+    if (numValue && textValue) {
+      factorObj[numValue] = textValue;
+    }
+  }
+  return factorObj;
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+function outputToPage(str) {
+  newEl = document.createElement('p');
+  newEl.innerHTML = str;
+  outputEl.appendChild(newEl);
 }
 
-// let's get this party started
-main();
+function fizzBuzzBoom(maxNums, factorObj) {
+  for (var num = 0; num < maxNums; num++) {
+    debugger;
+    var outputStr = "";
+    for (var factor in factorObj) {
+      if (num % factor == 0) {
+        outputStr += factorObj[factor];
+      }
+    }
+    if (outputStr) {
+      outputStr = " - " + outputStr + "!";
+    }
+    outputToPage(num.toString() + outputStr)
+  }
+}
+
+function reportError(str) {
+  outputEl.innerHTML = "<div class ='error'>" + str + "</div>";
+}
+
+document.getElementById('submit').addEventListener('click', function() {
+  var max = document.getElementById('max').value;
+  console.log('max:', max)
+  // if (!max) {
+  //   reportError("You must provide a maximum");
+  // } else {
+    var factors = {
+      3: "Fizz",
+      5: "Buzz",
+      7: "Boom"
+    };
+    fizzBuzzBoom(parseInt(max), factors);
+  }
+);
